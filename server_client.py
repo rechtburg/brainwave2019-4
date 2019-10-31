6e983680871c7f04e3d28f8cbecd3bb3e46439c7
山下優樹 <yukiyama913@gmail.com>
2019-10-31 13:22:29 +0900

    #8 不要なimport randomを削除

--- server_client.py
+++ server_client.py
@@ -1,27 +1,24 @@
+# server.pyの動作確認用のクライアント
 import argparse
-import random
 import time
 
 from pythonosc import osc_message_builder
 from pythonosc import udp_client
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--ip", default="127.0.0.1",
-                        help="The ip of the OSC server")
-    parser.add_argument("--port", type=int, default=5000,
-                        help="The port the OSC server is listening on")
-    args = parser.parse_args()
+parser = argparse.ArgumentParser()
+parser.add_argument("--ip", default="127.0.0.1", help="The ip of the OSC server")
+parser.add_argument("--port", type=int, default=5000, help="The port the OSC server is listening on")
+args = parser.parse_args()
 
-    client = udp_client.UDPClient(args.ip, args.port)
+client = udp_client.UDPClient(args.ip, args.port)
 
-    for x in range(10):
-        msg = osc_message_builder.OscMessageBuilder(address='/muse/elements/beta_relative')
-        msg.add_arg("ch1")
-        msg.add_arg("ch2")
-        msg.add_arg("ch3")
-        msg.add_arg("ch4")
-        msg = msg.build()
-        print("Sending", msg.dgram)
-        client.send(msg)
-        time.sleep(1)
+for x in range(10):
+    msg = osc_message_builder.OscMessageBuilder(address='/muse/elements/beta_relative/')
+    msg.add_arg("ch1")
+    msg.add_arg("ch2")
+    msg.add_arg("ch3")
+    msg.add_arg("ch4")
+    msg = msg.build()
+    print("Sending", msg.dgram)
+    client.send(msg)
+    time.sleep(1)
