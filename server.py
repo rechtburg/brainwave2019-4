234ff16efafce5cb9b00f9f94cf2b67c7e9b6b44
山下優樹 <yukiyama913@gmail.com>
2019-10-31 13:14:42 +0900

    #8 if __name__ == __main__: にする必要がないので削除

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+# 教師データ取得時に使用するサーバ
 import argparse
 from pythonosc.dispatcher import Dispatcher
 from pythonosc import osc_server
@@ -7,25 +8,24 @@ import time
 t = time.time()
 
 def set_filter(address: str, *args: List[Any]) -> None:
-    # We expect two float arguments
+    # We expect four arguments
     if len(args) != 4:
         return
 
     # Check that address starts with filter
-    if address[:28] != '/muse/elements/beta_relative':
+    if address[:29] != '/muse/elements/beta_relative/':
         return
 
     print(f'{time.time() - t}, {args[0]}, {args[1]}, {args[2]}, {args[3]}')
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--ip", default="127.0.0.1", help="The ip to listen on")
-    parser.add_argument("--port", type=int, default=5000, help="The port to listen on")
-    args = parser.parse_args()
+parser = argparse.ArgumentParser()
+parser.add_argument("--ip", default="127.0.0.1", help="The ip to listen on")
+parser.add_argument("--port", type=int, default=5000, help="The port to listen on")
+args = parser.parse_args()
 
-    dispatcher = Dispatcher()
-    dispatcher.map("/muse/elements/beta_relative*", set_filter)  # Map wildcard address to set_filter function
+dispatcher = Dispatcher()
+dispatcher.map("/muse/elements/beta_relative*", set_filter)  # Map wildcard address to set_filter function
 
-    server = osc_server.ThreadingOSCUDPServer((args.ip, args.port), dispatcher)
-    print("Serving on {}".format(server.server_address))
-    server.serve_forever()
+server = osc_server.ThreadingOSCUDPServer((args.ip, args.port), dispatcher)
+print("Serving on {}".format(server.server_address))
+server.serve_forever()
