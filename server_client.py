3775481f17b22c8a84c646c3f470184e2d3f4029
山下優樹 <yukiyama913@gmail.com>
2019-10-31 17:00:18 +0900

    #8 Processing動作確認のため軽微な変更

--- server_client.py
+++ server_client.py
@@ -1,6 +1,7 @@
 # server.pyの動作確認用のクライアント
 import argparse
 import time
+import random
 
 from pythonosc import osc_message_builder
 from pythonosc import udp_client
@@ -12,12 +13,12 @@ args = parser.parse_args()
 
 client = udp_client.UDPClient(args.ip, args.port)
 
-for x in range(10):
-    msg = osc_message_builder.OscMessageBuilder(address='/muse/elements/beta_relative/')
-    msg.add_arg("ch1")
-    msg.add_arg("ch2")
-    msg.add_arg("ch3")
-    msg.add_arg("ch4")
+for x in range(60):
+    msg = osc_message_builder.OscMessageBuilder(address='/muse/elements/beta_relative')
+    msg.add_arg(random.random())
+    msg.add_arg(random.random())
+    msg.add_arg(random.random())
+    msg.add_arg(random.random())
     msg = msg.build()
     print("Sending", msg.dgram)
     client.send(msg)
